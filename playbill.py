01034ca0a37e88b993b19581da9bff17eac73360
Sergey <piteryo7@gmail.com>
2018-11-10 00:04:03 +0300

    add select bills method

--- playbill.py
+++ playbill.py
@@ -3,21 +3,22 @@ import datetime
 
 import pandas as pd
 
-tea = pd.read_csv(open("Концерты Чайковский.csv", 'r', encoding='utf-8'), encoding="utf-8", parse_dates=[1,2])
-rah = pd.read_csv(open("Концерты_Рахманинов.csv", 'r', encoding='utf-8'), encoding="utf-8", parse_dates=[1,2])
+tea = pd.read_csv(open("Концерты Чайковский.csv", 'r', encoding='utf-8'), encoding="utf-8", parse_dates=[1, 2])
+rah = pd.read_csv(open("Концерты_Рахманинов.csv", 'r', encoding='utf-8'), encoding="utf-8", parse_dates=[1, 2])
 res = pd.concat([tea, rah])
-#print(pd.to_datetime(res["дата, гггг-мм-дд"]).head())
-
 res["дата, гггг-мм-дд"] = pd.to_datetime(res["дата, гггг-мм-дд"])
-#res["время, чч-мм"] = pd.to_datetime(res["время, чч-мм"] )
 
-print(res.dtypes)
 
-def find_concerts(composers, date=None):
-    op = operator.ge if date == None else operator.eq
-    if date == None:
-        date = pd.to_datetime('today')
-    
-    res["дата, гггг-мм-дд"]
+def find_concerts(composers, start_date=None, end_date=None):
+    global res
+    if start_date is None:
+        start_date = pd.to_datetime('today')
 
-    return None
+    res = res[res["дата, гггг-мм-дд"] >= start_date]
+    if end_date:
+        res = res[res["дата, гггг-мм-дд"] <= end_date]
+
+    res = res[res['программа'].str.contains('|'.join(composers))]
+    return res.to_dict
+
+# print(find_concerts(["Свиридов", "Чайковский"], datetime.datetime(2019, 2, 24, 0, 0)))
