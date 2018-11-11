132a56cb321e43b5de938fe9e380c2a250e0bd00
Yury Belousov <yury-belousov@yandex.ru>
2018-11-11 04:28:17 +0300

    clear context after faq

--- info.py
+++ info.py
@@ -4,6 +4,11 @@ import pandas as pd
 import pymorphy2
 import nltk
 from faq import get_faq_response
+
+import coloredlogs, logging
+coloredlogs.install()
+
+
 morph = pymorphy2.MorphAnalyzer()
 
 composerDescriptions = yaml.load(open('composerDescriptions.yaml', 'r', encoding="utf-8"))
@@ -15,20 +20,24 @@ def alice_info_endpoint(request, response, user_storage):
     composers = request.get_last_names(capitalize=False) & set(validComposers)
 
     word_tokens = request.get_tokens()
-    if len(composers) == 1 or 'composer' in user_storage and 'он' in [morph.parse(word)[0].normal_form for word in word_tokens]:
+    word_normal_forms = [morph.parse(word)[0].normal_form for word in word_tokens]
+    if len(composers) == 1 or 'composer' in user_storage and 'он' in word_normal_forms:
         if len(composers) == 1:
             user_storage['composer'] = composers.pop()
             response.set_text(get_info(request.command, user_storage['composer']))
         else:
-            for i, word in enumerate(word_tokens):
-                if morph.parse(word)[0].normal_form == 'он':
+            for i in range(len(word_tokens)):
+                if word_normal_forms[i] == 'он':
                     word_tokens[i] = user_storage['composer']
             response.set_text(get_info(' '.join(word_tokens), user_storage['composer']))
         return response, user_storage
+
+    user_storage.pop('composer', None)
     response.set_text(get_faq_response(request.command))
     return response, user_storage
 
 def get_info(request, composer="чайковский"):
+    logging.error(f"get info from composer command: {request}")
     r = requests.post(pavlovUrls['squad_path'], json={
         'context': composerDescriptions[composer],
         'question': [request]
